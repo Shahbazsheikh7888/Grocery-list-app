68205cb3f1abcf516befd9e1c57bc3a56de61def
SHAHBAZ SHEIKH <114724299+Shahbazsheikh7888@users.noreply.github.com>
2022-12-26 18:08:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const groceries = document.getElementsByClassName("groceries")[0];
+const pencil = document.getElementById("pencil");
+const allitems = document.getElementById("allitems");
+const userInput = document.getElementById("userinput");
+
+pencil.addEventListener("click", function () {
+  allitems.innerHTML = "";
+});
+userInput.addEventListener("keydown", function (event) {
+  if (event.key == "Enter") additem();
+});
+function additem() {
+  var h2 = document.createElement("h2");
+  h2.innerHTML = "-" + userInput.value;
+
+  h2.addEventListener("click", function () {
+    h2.stlye.textDecoration = "line-through";
+
+    
+  })
+  allitems.insertAdjacentElement("beforeend", h2);
+    userInput.value = "";
+}
